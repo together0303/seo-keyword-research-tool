deb03eca01edc50a772bfcbd8885b6f9ea3009f4
stogether0303 <stogether0303@gmail.com>
2024-02-14 09:20:22 +0200

    add metadata preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,10 @@ import { Suspense } from 'react';
 
 export const metadata = {
   title: 'Rankulate - Harnessing KD Research and Competitive Analysis Tools',
-  description:
-    'ankulate offers cutting-edge SEO tools designed to leverage in-depth KD research and competitive analysis, empowering businesses to enhance their online visibility and drive organic traffic. Stay ahead of the competition with Rankulate\'s comprehensive suite of SEO solutions'
+  description: 'ankulate offers cutting-edge SEO tools designed to leverage in-depth KD research and competitive analysis, empowering businesses to enhance their online visibility and drive organic traffic. Stay ahead of the competition with Rankulate\'s comprehensive suite of SEO solutions',
+  openGraph: {
+    images: '/logo.png',
+  },
 };
 
 export default function RootLayout({
